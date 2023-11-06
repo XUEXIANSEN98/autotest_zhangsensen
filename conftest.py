7610f2b294e78e44402544801ae74c5c74d5aa30
XUEXIANSEN98 <xuexiansensen@163.com>
2023-11-06 14:42:39 +0800

    首次添加

--- conftest.py
+++ conftest.py
@@ -1,5 +1,5 @@
 import pytest
-from Apiframework.common.yaml_util import YamlUtil
+from common.yaml_util import YamlUtil
 
 # 使用fixture固件，作用于conftest文件，每次会话执行前都执行一遍该方法
 @pytest.fixture(scope="session", autouse=True)
